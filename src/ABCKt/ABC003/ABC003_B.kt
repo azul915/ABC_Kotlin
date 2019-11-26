3d9acb3ded53da3b3756248bf2bdddb7d067859b
azul915 <k.aoyagi940516@gmail.com>
2019-11-27 00:37:45 +0900

    ABC003_B

--- src/ABCKt/ABC003/ABC003_B.kt
+++ src/ABCKt/ABC003/ABC003_B.kt
@@ -1,4 +1,11 @@
 fun ABC003_B() {
-    val (S, T) = readLine()!!.split(" ")
-
+    val (S, T) = (1..2).map { readLine()!! }
+    val atcoder = listOf('a','t','c','o','d','e','r','@')
+    (S.indices).all {
+        when(S[it]) {
+            '@' -> T[it] in atcoder
+            in atcoder -> T[it] == '@' || T[it] == S[it]
+            else -> T[it] == S[it]
+        }
+    }.let { if(it) println("You can win") else println("You will lose") }
 }
