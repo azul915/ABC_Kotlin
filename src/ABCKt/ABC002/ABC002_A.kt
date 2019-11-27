9791c491a30772d572f7a3f4d1ca2aeee03c7a70
azul915 <k.aoyagi940516@gmail.com>
2019-11-28 00:27:00 +0900

    ABC002_Aをkotlin.collections.Iterable#max()で実装

--- src/ABCKt/ABC002/ABC002_A.kt
+++ src/ABCKt/ABC002/ABC002_A.kt
@@ -1,6 +1,6 @@
 package ABCKt.ABC002
 
 fun ABC002_A() {
-    val (x, y) = readLine()!!.split(" ").map { it.toInt() }
-    if (x > y) println(x) else println(y)
+    val bigger = readLine()!!.split(" ").map { it.toInt() }.max()!!
+    println(bigger)
 }
