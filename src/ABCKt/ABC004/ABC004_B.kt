558a551a809d4131454d7d465d8752198c497dc5
azul915 <k.aoyagi940516@gmail.com>
2019-11-28 02:02:05 +0900

    ABC004_B型情報

--- src/ABCKt/ABC004/ABC004_B.kt
+++ src/ABCKt/ABC004/ABC004_B.kt
@@ -1,5 +1,5 @@
 fun ABC004_B() {
-    val box = (0..3).map { readLine()!!.split(" ") }
-    val reversed = box.map { it.reversed() }.reversed()
+    val box: List<List<String>> = (0..3).map { readLine()!!.split(" ") }
+    val reversed: List<List<String>> = box.map { it.reversed() }.reversed()
     reversed.map { it.joinToString(" ") }.forEach { println(it) }
 }
