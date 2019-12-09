aeedb9aee0d9a74283ecf2305e5146f489f7c7d0
azul915 <k.aoyagi940516@gmail.com>
2019-12-10 08:49:41 +0900

    Add ABC010_A, ABC011_A

---
+++ src/ABCKt/ABC010/ABC010_A.kt
@@ -0,0 +1,3 @@
+fun ABC010_A() {
+    println("${readLine()!!}pp")
+}
---
+++ src/ABCKt/ABC011/ABC011_A.kt
@@ -0,0 +1,7 @@
+fun ABC011_A() {
+
+    var input = readLine()!!.toInt()
+    val next = if(input <= 12) input++ else 13
+    println(next)
+
+}
