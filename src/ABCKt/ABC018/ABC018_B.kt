bc4de78b85ceedf2d2589518026e2605fae38a08
azul915 <k.aoyagi940516@gmail.com>
2020-01-03 22:36:38 +0900

    Add ABC018_B

---
+++ src/ABCKt/ABC018/ABC018_B.kt
@@ -0,0 +1,11 @@
+// TODO: immutableにする
+fun ABC018_B() {
+    var S = readLine()!!
+    val N = readLine()!!.toInt()
+    var lr: List<Int>
+    for(i in 1..N) {
+        lr = readLine()!!.split(" ").map { it.toInt() }
+        S = S.substring(0, lr[0] -1) + S.substring(lr[0] -1, lr[1]).toCharArray().reversed().joinToString(separator = "") + S.substring(lr[1], S.length)
+    }
+    println(S)
+}
