523ef0e4d10d8d00d69094356dcd5c7796b9eb7e
azul915 <k.aoyagi940516@gmail.com>
2020-01-17 02:17:46 +0900

    Update ABC018_B

--- src/ABCKt/ABC018/ABC018_B.kt
+++ src/ABCKt/ABC018/ABC018_B.kt
@@ -5,7 +5,7 @@ fun ABC018_B() {
     var lr: List<Int>
     for(i in 1..N) {
         lr = readLine()!!.split(" ").map { it.toInt() }
-        S = S.substring(0, lr[0] -1) + S.substring(lr[0] -1, lr[1]).toCharArray().reversed().joinToString(separator = "") + S.substring(lr[1], S.length)
+        S = S.substring(0, lr[0] -1) + S.substring(lr[0] -1, lr[1]).toCharArray().reversed().joinToString("") + S.substring(lr[1], S.length)
     }
     println(S)
 }
