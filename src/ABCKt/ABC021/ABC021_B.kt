dad0632a73c2a825838b1bd24f0e484f8ac4b116
azul915 <k.aoyagi940516@gmail.com>
2020-06-07 20:42:07 +0900

    Add ABC021_B

---
+++ src/ABCKt/ABC021/ABC021_B.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    readLine()!!
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    readLine()!!
+    val k = readLine()!!.split(" ").map { it.toInt() }
+    if (k.contains(a) || k.contains(b) || k.size != k.distinct().size) {
+        println("NO")
+    } else {
+        println("YES")
+    }
+}
