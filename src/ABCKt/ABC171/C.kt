f25394c121cf22dfe7831e9626c17227838c694a
azul915 <k.aoyagi940516@gmail.com>
2020-06-21 22:55:26 +0900

    Add ABC171 A,B andC

---
+++ src/ABCKt/ABC171/C.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var n = readLine()!!.toLong()
+    val az = arrayOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+    var result = StringBuilder()
+
+    while (n > 0) {
+        val h = (n % 26).toInt()
+        if (h == 0) {
+            result.append(az[25])
+            n--
+        } else {
+            val s = h
+            result.append(az[s - 1])
+        }
+        n /= 26
+    }
+    val output = result.split("").reversed().joinToString("")
+    println(output)
+}
