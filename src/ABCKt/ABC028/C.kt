08b86b21af5a73519456da77dea440011019475a
azul915 <k.aoyagi940516@gmail.com>
2021-11-18 00:27:10 +0900

    update

--- src/ABCKt/ABC028/C.kt
+++
@@ -1,6 +0,0 @@
-import kotlin.math.max
-
-fun main() {
-    val input = readLine()!!.split(" ").map { it.toInt() }
-    println(max(input[0]+input[3]+input[4], input[1]+input[2]+input[4]))
-}
--- src/ABCKt/ABC061/B.kt
+++ src/ABCKt/ABC061/B.kt
@@ -1,10 +1,10 @@
-fun main() {
-    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
-    val arr = IntArray(n)
-    for (i in 1..m) {
-        val (a, b) = readLine()!!.split(" ").map { it.toInt() }
-        arr[a-1]++
-        arr[b-1]++
-    }
-    arr.forEach { println(it) }
-}
+//fun main() {
+//    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+//    val arr = IntArray(n)
+//    for (i in 1..m) {
+//        val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+//        arr[a-1]++
+//        arr[b-1]++
+//    }
+//    arr.forEach { println(it) }
+//}
