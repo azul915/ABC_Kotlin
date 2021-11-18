434b5214396b1dbcebef30a4d3fb17c4a880fa6d
azul915 <k.aoyagi940516@gmail.com>
2021-11-19 00:59:54 +0900

    add two-sum

--- src/LeetCode/EasyAcceptanceUnder50.kt
+++ src/LeetCode/EasyAcceptanceUnder50.kt
@@ -1,3 +1,15 @@
 package LeetCode
 
 fun test(): String = "EasyAcceptanceUnder50"
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    var m = mutableMapOf<Int, Int>()
+    for (i in nums.indices) {
+        val complement = target - nums[i]
+        if (m.containsKey(complement)) {
+            return intArrayOf(m[complement]!!, i)
+        }
+        m[nums[i]] = i
+    }
+    return intArrayOf()
+}
