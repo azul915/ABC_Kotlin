a94652f974d5b8b00c4ff6acee5f9266026cfa95
azul915 <k.aoyagi940516@gmail.com>
2021-11-19 23:55:17 +0900

    add remove-duplicates-from-sorted-list

--- src/LeetCode/EasyAcceptanceUnder50.kt
+++ src/LeetCode/EasyAcceptanceUnder50.kt
@@ -13,3 +13,19 @@ fun twoSum(nums: IntArray, target: Int): IntArray {
     }
     return intArrayOf()
 }
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun deleteDuplicates(head: ListNode?): ListNode? {
+    var current = head
+    while (current?.next != null) {
+        if (current.`val` == current?.next?.`val`) {
+            current.next = current?.next?.next
+        } else {
+            current = current.next
+        }
+    }
+    return head
+}
