3318fa0bb0e04a842b4838c8d11b59e684fb9573
azul915 <k.aoyagi940516@gmail.com>
2022-01-02 00:11:06 +0900

    fix 1013

--- src/main.kt
+++ src/main.kt
@@ -4,10 +4,10 @@ import kotlin.test.assertEquals
 
 fun main() {
     listOf(
-        //Pair<Boolean, IntArray>(true, intArrayOf(0,2,1,-6,6,-7,9,1,2,0,1)),
-        //Pair<Boolean, IntArray>(false, intArrayOf(0,2,1,-6,6,7,9,-1,2,0,1)),
-        //Pair<Boolean, IntArray>(true, intArrayOf(3,3,6,5,-2,2,5,1,-9,4)))
-        Pair<Boolean, IntArray>(false, intArrayOf(1,-1,1,-1)))
-        .forEach { assertEquals(it.first, canThreePartsEqualSum(it.second)) }
+        Pair<Boolean, IntArray>(true, intArrayOf(0,2,1,-6,6,-7,9,1,2,0,1)),
+        Pair<Boolean, IntArray>(false, intArrayOf(0,2,1,-6,6,7,9,-1,2,0,1)),
+        Pair<Boolean, IntArray>(true, intArrayOf(3,3,6,5,-2,2,5,1,-9,4)),
+        Pair<Boolean, IntArray>(false, intArrayOf(1,-1,1,-1))
+    ).forEach { assertEquals(it.first, canThreePartsEqualSum(it.second), "${it.second.contentToString()}") }
 
 }
